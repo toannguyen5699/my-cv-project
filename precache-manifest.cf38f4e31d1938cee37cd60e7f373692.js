240b23894c0845518b93d4486d77db4652a4ad51
toan <toannguyen050699@gmail.com>
2020-05-04 10:42:55 +0700

    Updates

---
+++ precache-manifest.cf38f4e31d1938cee37cd60e7f373692.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a029529c5badddaf0c3",
+    "url": "/my-cv-project/static/css/main.31d9acca.chunk.css"
+  },
+  {
+    "revision": "6a029529c5badddaf0c3",
+    "url": "/my-cv-project/static/js/main.494c9341.chunk.js"
+  },
+  {
+    "revision": "b0f128f7e3161a74e84c",
+    "url": "/my-cv-project/static/js/runtime~main.bdef3713.js"
+  },
+  {
+    "revision": "9abfaad7185066226db4",
+    "url": "/my-cv-project/static/js/2.bd4e3280.chunk.js"
+  },
+  {
+    "revision": "ef468e275a2c13d0bc7156549261fde6",
+    "url": "/my-cv-project/static/media/Octocat.ef468e27.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/my-cv-project/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "7291d1b17a8349fdfd283a9f0cf9dea6",
+    "url": "/my-cv-project/static/media/facebook-logo.7291d1b1.jpg"
+  },
+  {
+    "revision": "755d440c3bef3bd0430d8d1f15de6597",
+    "url": "/my-cv-project/static/media/CTB.755d440c.jpg"
+  },
+  {
+    "revision": "86dadff996db18d26f0f571ac6234c2f",
+    "url": "/my-cv-project/static/media/pluralsight.86dadff9.png"
+  },
+  {
+    "revision": "fab1e598a0654f6bea84248370184fdb",
+    "url": "/my-cv-project/static/media/background.fab1e598.jpg"
+  },
+  {
+    "revision": "564e4bafcd3b130210a1a9bfb32995a5",
+    "url": "/my-cv-project/static/media/SG-Logo.564e4baf.png"
+  },
+  {
+    "revision": "8c3064d9010baecd0fa1619bc4fa0ac7",
+    "url": "/my-cv-project/static/media/garmin.8c3064d9.png"
+  },
+  {
+    "revision": "57613c06e4656fabeab5e1eff849adcc",
+    "url": "/my-cv-project/index.html"
+  }
+];
